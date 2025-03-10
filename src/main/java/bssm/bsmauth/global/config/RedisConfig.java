57c57e29df7ce1ee3474b5d37b52e0864b9a5fe3
leehj050211 <leehj050211@gmail.com>
2025-03-10 17:12:20 +0900

    FIX: redis 연결 설정

--- src/main/java/bssm/bsmauth/global/config/RedisConfig.java
+++ src/main/java/bssm/bsmauth/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,10 +16,21 @@ public class RedisConfig {
 
     @Value("${spring.redis.host}")
     private String host;
+
     @Value("${spring.redis.port}")
     private int port;
+
     @Value("${spring.redis.database}")
-    private int database;
+    private int databaseIndex;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setDatabase(databaseIndex);
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
