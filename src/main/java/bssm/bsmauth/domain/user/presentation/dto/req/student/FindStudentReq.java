a22bdd3ce1deac095174e754a217ab5dd037dd77
leehj050211 <leehj050211@gmail.com>
2025-04-24 12:31:39 +0900

    FIX: FindStudentReq.java

--- src/main/java/bssm/bsmauth/domain/user/presentation/dto/req/student/FindStudentReq.java
+++ src/main/java/bssm/bsmauth/domain/user/presentation/dto/req/student/FindStudentReq.java
@@ -1,5 +1,8 @@
 package bssm.bsmauth.domain.user.presentation.dto.req.student;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FindStudentReq {
 
+    @Min(1)
+    @Max(3)
     private int grade;
+
+    @Min(1)
+    @Max(4)
     private int classNo;
+
+    @Min(1)
+    @Max(17)
     private int studentNo;
+
+    @NotBlank
     private String name;
 }
